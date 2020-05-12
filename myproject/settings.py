1993ac996323f3eb350a3c166d75832645d7caa2
Javier E Jaramillo <jjaramillo34@gmail.com>
2020-05-12 14:42:32 -0400

    make it better

--- myproject/settings.py
+++ myproject/settings.py
@@ -284,8 +284,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Heroku: Update database configuration from $DATABASE_URL.
 import dj_database_url
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
-
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 
 
