7646d5e9216aa1528d0a9a63bfdb4a65683a0120
jjaramillo34 <51845122+jjaramillo34@users.noreply.github.com>
2020-05-12 21:46:20 -0400

    Update models.py

--- news/models.py
+++ news/models.py
@@ -35,7 +35,7 @@ class News(models.Model):
 
 	def get_absolute_url(self):
         """Returns the url to access a detail record for this book."""
-        return reverse('book-detail', args=[str(self.id)])
+        return reverse('news', args=[str(self.slug)])
 
 	def __str__ (self):
 		return self.article_name
